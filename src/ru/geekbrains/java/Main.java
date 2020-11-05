5bfc3bfc4249343cb7b96c77d735ff584ce2cb32
paul <paulslp@yandex.ru>
2020-11-05 15:52:43 +0400

    lesson03 исправление лишнего сообщения при переходе к 2 заданию

--- src/ru/geekbrains/java/Main.java
+++ src/ru/geekbrains/java/Main.java
@@ -8,39 +8,14 @@ public class Main {
     public static void main(String[] args) {
         Random random = new Random();
         Scanner scanner = new Scanner(System.in);
+
         guessNumber(random, scanner);
 
         String[] words = {"apple", "orange", "lemon", "banana", "apricot", "avocado",
                 "broccoli", "carrot", "cherry", "garlic", "grape", "melon", "leak", "kiwi",
                 "mango", "mushroom", "nut", "olive", "pea", "peanut", "pear", "pepper",
                 "pineapple", "pumpkin", "potato"};
-        System.out.println("Введите слово: ");
         System.out.println("Вы " + (guessWordAttempt(scanner, words[random.nextInt(words.length)]) ? "выиграли" : "проиграли"));
-    }
-
-    private static boolean guessWordAttempt(Scanner scanner, String hiddenWord) {
-        do {
-            String answerWord = scanner.nextLine();
-            if (answerWord.equals(hiddenWord)) {
-                return true;
-            } else {
-                System.out.println("Загаданное слово:" + matchAnswerWithHiddenWord(answerWord, hiddenWord));
-            }
-        } while (true);
-
-    }
-
-    private static String matchAnswerWithHiddenWord(String answerWord, String hiddenWord) {
-        String result = "###############";
-        int minWordLength = Math.min(hiddenWord.length(), answerWord.length());
-        for (int i = 0; i < minWordLength; i++) {
-            if (answerWord.charAt(i) == hiddenWord.charAt(i)) {
-                char[] charResult = result.toCharArray();
-                charResult[i] = hiddenWord.charAt(i);
-                result = String.valueOf(charResult);
-            }
-        }
-        return result;
     }
 
     private static void guessNumber(Random random, Scanner scanner) {
@@ -70,5 +45,31 @@ public class Main {
             }
         } while (numberOfAttempts < 3);
         return false;
+    }
+
+    private static boolean guessWordAttempt(Scanner scanner, String hiddenWord) {
+        do {
+            System.out.println("Введите слово: ");
+            String answerWord = scanner.next();
+            if (answerWord.equals(hiddenWord)) {
+                return true;
+            } else {
+                System.out.println("Загаданное слово:" + matchAnswerWithHiddenWord(answerWord, hiddenWord));
+            }
+        } while (true);
+
+    }
+
+    private static String matchAnswerWithHiddenWord(String answerWord, String hiddenWord) {
+        String result = "###############";
+        int minWordLength = Math.min(hiddenWord.length(), answerWord.length());
+        for (int i = 0; i < minWordLength; i++) {
+            if (answerWord.charAt(i) == hiddenWord.charAt(i)) {
+                char[] charResult = result.toCharArray();
+                charResult[i] = hiddenWord.charAt(i);
+                result = String.valueOf(charResult);
+            }
+        }
+        return result;
     }
 }
