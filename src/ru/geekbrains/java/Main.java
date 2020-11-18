eb656c460e7af5977b0362ffbdd212af86586859
paul <paulslp@yandex.ru>
2020-11-18 17:31:45 +0400

    lesson07 вынес функционал метода main  в отдельные методы, и вызвал эти методы в main

--- src/ru/geekbrains/java/Main.java
+++ src/ru/geekbrains/java/Main.java
@@ -3,11 +3,27 @@ package ru.geekbrains.java;
 public class Main {
 
     public static void main(String[] args) {
-        Cat[] cats = {new Cat("Barsik", 5), new Cat("Chris", 6), new Cat("Denis", 4),
-                new Cat("Lemon", 3), new Cat("Tom", 7), new Cat("Robert", 10)};
+        Cat[] cats = createAndFillAndGetCatsArray();
+
         Plate plate = new Plate(25);
         System.out.println("Information about plate before eating:");
         plate.info();
+
+        eating(cats, plate);
+
+        addingFoodToPlate(plate, 10, 25);
+    }
+
+    private static Cat[] createAndFillAndGetCatsArray() {
+        return new Cat[]{new Cat("Barsik", 5),
+                new Cat("Chris", 6),
+                new Cat("Denis", 4),
+                new Cat("Lemon", 3),
+                new Cat("Tom", 7),
+                new Cat("Robert", 10)};
+    }
+
+    private static void eating(Cat[] cats, Plate plate) {
         System.out.println("Start eating:");
         for (int i = 0; i < cats.length; i++) {
             cats[i].eat(plate);
@@ -15,9 +31,12 @@ public class Main {
             plate.info();
         }
         System.out.println("Finish eating:");
-        if (plate.getFood() < 10) {
-            plate.addFood(25);
-            System.out.println("Add 25 food units");
+    }
+
+    private static void addingFoodToPlate(Plate plate, int minFoodCount, int foodCount) {
+        if (plate.getFood() < minFoodCount) {
+            plate.addFood(foodCount);
+            System.out.println("Add " + foodCount + " food units");
         }
         plate.info();
     }
